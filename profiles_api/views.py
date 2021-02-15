a91e8d10571a9dd3ab92cf8da4ef09d1a6c08de2
shreya <shreeeee96@gmail.com>
2021-02-15 21:58:12 +0530

    authentication done

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+#from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 
 from profiles_api import serializer
@@ -112,3 +114,20 @@ class UserLoginApiView(ObtainAuthToken):
     '''Handle creating user authentucation tokens'''
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     
+    
+  
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    '''Hnadles Creating,updating, reading profile feed items'''
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializer.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        #IsAuthenticatedOrReadOnly 
+        IsAuthenticated        
+    )
+    
+    
+    def perform_create(self, serializer):
+        '''Sets the user profile to the logged in user'''
+        serializer.save(user_profile=self.request.user)
